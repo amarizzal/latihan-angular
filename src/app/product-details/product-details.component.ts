55420ea0eed4e6ad61244027664168d322cc0420
amarizzal <rizal.ammar91@yahoo.com>
2021-01-14 19:27:24 +0700

    2 files modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { products } from "../products";
+import { CartServiceService } from "../cart-service.service";
 
 @Component({
   selector: "app-product-details",
@@ -10,12 +11,19 @@ import { products } from "../products";
 })
 export class ProductDetailsComponent implements OnInit {
   product;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartServiceService: CartServiceService
+  ) {}
 
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get("productId"));
 
     this.product = products.find(product => product.id == productIdFromRoute);
+  }
+
+  addToCart(product) {
+    this.cartServiceService.addProduct(product);
   }
 }
